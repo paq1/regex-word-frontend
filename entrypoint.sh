0bf937b25bda317318f7dd4ed5c9f1941dbc6f81
paq1 <p.paquin1605@gmail.com>
2025-01-10 06:18:12 +0100

    ci: add debug on entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,23 @@
 #!/bin/sh
 
+echo "Starting entrypoint script..."
+
+# Debug : Vérifie si les variables d'environnement sont présentes
+echo "Environment variables:"
+env
+
 # Chemin du fichier env-config.js
 ENV_CONFIG_FILE="/usr/share/nginx/html/assets/env-config.js"
+echo "Writing environment variables to $ENV_CONFIG_FILE"
 
 # Remplacement des placeholders par les valeurs des variables d'environnement
-cat <<EOF > /usr/share/nginx/html/assets/env-config.js
+cat <<EOF > $ENV_CONFIG_FILE
 (function (window) {
   window.__env = window.__env || {};
   window.__env.VAR_TEST = "${VAR_TEST}";
 })(this);
+EOF
 
+# Debug : Vérifie si le fichier a été généré correctement
+echo "Generated $ENV_CONFIG_FILE:"
+cat $ENV_CONFIG_FILE
